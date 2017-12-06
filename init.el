59a7bc3b92e63d037310d3e941d51a31532aba2b
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2017-12-06 22:44:47 +0100

    Copy some package declarations from the interwebz

--- init.el
+++ init.el
@@ -29,6 +29,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
  '(delete-old-versions t)
  '(global-linum-mode t)
  '(show-paren-mode t)
@@ -41,10 +44,81 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Pro")))))
 
 (use-package auto-compile
   :config (auto-compile-on-load-mode))
 
 (setq load-prefer-newer t)
 
+;; Checks for package update
+(use-package auto-package-update
+  :config
+  (auto-package-update-maybe)
+  (setq auto-package-update-delete-old-versions t))
+
+;; Nice mode line, with nice theme
+;(use-package smart-mode-line-powerline-theme
+;  :config (setq sml/theme 'powerline))
+(use-package smart-mode-line
+  :config
+  (display-time-mode)
+  (sml/setup))
+
+;; Code completion
+(use-package company
+  :config (add-hook 'after-init-hook 'global-company-mode))
+
+;; Additional completion packages
+(use-package company-ansible)
+
+(use-package magit)
+
+;; Helm for minibuffer completion
+(use-package helm
+  :config (helm-mode 1)
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring))
+
+;; Minimap
+(use-package sublimity
+  :config
+  (require 'sublimity)
+  (require 'sublimity-scroll)
+  (require 'sublimity-map)
+  (setq sublimity-map-size 20)
+  (setq sublimity-map-fraction 0.3)
+  (setq sublimity-map-text-scale -7)
+  ;(require 'sublimity-attractive)
+  (sublimity-mode 1)
+  (sublimity-map-set-delay 0)
+  )
+
+;; Use custom theme
+(use-package monokai-theme
+  :config (load-theme 'monokai t))
+
+;; Multi terminal emulation
+(use-package multi-term
+    :config (require 'multi-term))
+
+(use-package markdown-mode)
+(use-package markdown-preview-mode)
+(use-package markdown-toc)
+(use-package json-mode)
+
+;; Bash completion setup
+(use-package bash-completion
+  :config (require 'bash-completion)
+  (bash-completion-setup))
+
+(use-package discover-my-major
+  :config (global-set-key (kbd "C-h C-m") 'discover-my-major)
+  (global-set-key (kbd "C-h M-m") 'discover-my-mode))
+
+;; Nice package to automatically disassemble java .class files
+(use-package autodisass-java-bytecode)
+
+;(use-package ocodo-svg-modelines
+;  :config
+;  (ocodo-svg-modelines-init))
