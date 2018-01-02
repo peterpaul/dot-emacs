37eb98402fcbde3746fbe61df985a147be36db41
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2018-01-02 19:59:41 +0100

    Use Source Code Pro instead of Variable

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t; -*-
 (require 'package) ;; You might already have this line
 
 (setq package-enable-at-startup nil)
@@ -47,7 +48,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Variable"))))
+ '(default ((t (:family "Source Code Pro"))))
  '(minimap-active-region-background ((t (:background "DodgerBlue4")))))
 
 (use-package auto-compile
