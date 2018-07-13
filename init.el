77d996026da117756a133c01ca94bb4991b76ced
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@connectis.nl>
2018-07-13 09:41:28 +0200

    Update todo and dashboard config

--- init.el
+++ init.el
@@ -197,6 +197,11 @@
   :config
   (require 'dashboard)
   (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
   )
 
 (use-package lastpass)
