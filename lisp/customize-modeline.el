a7db4e7509902bb5c20acd763a0014faeffb409d
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@connectis.nl>
2018-07-27 14:02:06 +0200

    Switch to treemacs, doom-one theme with airline.

--- lisp/customize-modeline.el
+++ lisp/customize-modeline.el
@@ -6,17 +6,32 @@
   ;;   (sml/setup)
   ;;   )
 
-  ;; (use-package powerline
-  ;;   :config
-  ;;   (powerline-default-theme)
-  ;;   )
+  (use-package powerline
+    :config
+    (progn
+      (setq powerline-utf-8-separator-left        #xe0b0
+	    powerline-utf-8-separator-right       #xe0b2
+	    airline-utf-glyph-separator-left      #xe0b0
+	    airline-utf-glyph-separator-right     #xe0b2
+	    airline-utf-glyph-subseparator-left   #xe0b1
+	    airline-utf-glyph-subseparator-right  #xe0b3
+	    airline-utf-glyph-branch              #xe0a0
+	    airline-utf-glyph-readonly            #xe0a2
+	    airline-utf-glyph-linenumber          #xe0a1)
+      (powerline-default-theme)
+      ))
+
+  (use-package airline-themes
+    :config
+    (load-theme 'airline-doom-one)
+    )
   
   ;; (defvar use-fancy-spaceline (y-or-n-p-with-timeout "Use fancy spaceline-all-the-icons?" 3 nil))
-  (defvar use-fancy-spaceline nil)
+  ;; (defvar use-fancy-spaceline nil)
 
-  (use-package spaceline
-    :config
-    (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
+  ;; (use-package spaceline
+  ;;   :config
+  ;;   (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
 
   ;; (use-package spaceline-config
   ;;   :config
@@ -26,17 +41,17 @@
   ;;     )
   ;;   )
 
-  (when use-fancy-spaceline
-    (use-package spaceline-all-the-icons
-      :after spaceline
-      :config
-      (spaceline-all-the-icons-theme)
-      (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
-      (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
-      (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
-      (setq spaceline-all-the-icons-separator-type (quote wave))
-      )
-    )
+  ;; (when use-fancy-spaceline
+  ;;   (use-package spaceline-all-the-icons
+  ;;     :after spaceline
+  ;;     :config
+  ;;     (spaceline-all-the-icons-theme)
+  ;;     (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
+  ;;     (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
+  ;;     (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
+  ;;     (setq spaceline-all-the-icons-separator-type (quote wave))
+  ;;     )
+  ;;   )
   )
 
 (provide 'customize-modeline)
