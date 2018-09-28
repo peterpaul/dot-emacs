be82b815478321759fb3e4d9063c286ba848106e
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@connectis.nl>
2018-09-28 23:02:41 +0200

    Add function to set value in calculate matrix

--- lisp/calculate.el
+++ lisp/calculate.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t; -*-
 (require 'matrix)
 
 (defun ilog10 (n)
@@ -35,13 +36,38 @@
 (defun calculate-new-sum (l)
   (list l (calculate-make-matrix l)))
 
+(defun calculate-set-value (calc i value)
+  (let* ((m    (nth 1 calc))
+	 (cols (matrix-cols m))
+	 (rows (matrix-rows m)))
+    (if (>= i (- (* 2 cols) 1))
+	(error "Invalid index %d" i)
+      (if (oddp i)
+	  (let ((r 0)
+		(c (- cols (+ 1 (/ i 2)) 1)))
+	    (matrix-set m c r value))
+	(let ((r (- rows 1))
+	      (c (- cols (/ i 2) 1)))
+	  (matrix-set m c r value))))))
+
+(defun calculate-render (calc)
+  (matrix-render (nth 1 calc)))
+
 (defun calculate-check-result (c r)
   (let ((expected (reduce '+ (nth 0 c))))
     (= expected r)))
 
+
+;; testscript
 (setq abc (list 1 23 456 6789))
 (calculate-check-result (calculate-new-sum abc) 7260) ; nil
 (calculate-check-result (calculate-new-sum abc) 7269) ; 't
-(matrix-render (calculate-make-matrix abc))
+(setq cal (calculate-new-sum abc))
+(calculate-render cal)
+
+(dotimes (i 9)
+  (calculate-set-value cal i i))
+
+(calculate-set-value cal 9 9)
 
 (provide 'calculate)
