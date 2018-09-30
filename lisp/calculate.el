de4682a51a7fee2ffe15670b8e7e4a9a464be356
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2018-09-30 22:22:39 +0200

    Refactorings

--- lisp/calculate.el
+++ lisp/calculate.el
@@ -36,16 +36,16 @@
 (defun calculate-new-sum (l)
   (list l (calculate-make-matrix l)))
 
-(defun calculate-max-index (calc)
-  (let* ((m    (nth 1 calc))
+(defun calculate-max-index ()
+  (let* ((m    (nth 1 *calculate-sum*))
 	 (cols (matrix-cols m)))
     (- (* 2 cols) 2)))
 
-(defun calculate-set-value (calc i value)
-  (let* ((m    (nth 1 calc))
+(defun calculate-set-value (i value)
+  (let* ((m    (nth 1 *calculate-sum*))
 	 (cols (matrix-cols m))
 	 (rows (matrix-rows m)))
-    (if (> i (calculate-max-index calc))
+    (if (> i (calculate-max-index))
 	(error "Invalid index %d" i)
       (if (oddp i)
 	  (let ((r 0)
@@ -55,24 +55,24 @@
 	      (c (- cols (/ i 2) 1)))
 	  (matrix-set m c r value))))))
 
-(defun calculate-render (calc)
+(defun calculate-render ()
   (let ((inhibit-read-only t))
     (erase-buffer)
-    (matrix-render (nth 1 calc))
+    (matrix-render (nth 1 *calculate-sum*))
     (if (calculate-finishedp)
-        (insert (format "Your answer is... %s"
-                        (if (calculate-check-result calc (calculate-get-result))
-                            "Correct!"
-                          "Incorrect...")))
+        (insert (if (calculate-check-result (calculate-get-result))
+                    "Dat is het juiste antwoord!\n\n"
+                  "Dat is fout, je moet beter je best doen op school.\n\n"))
       (if (oddp *calculate-index*)
-          (insert "Hoeveel onthouden?")
-        (insert "Hoeveel opschrijven?")))))
+          (insert "Hoeveel onthouden?\n\n")
+        (insert "Hoeveel opschrijven?\n\n")))
+    (insert "r - Begin opnieuw\nq - Stop rekensom\n")))
 
 (defun calculate-finishedp ()
-  (> *calculate-index* (calculate-max-index *calculate-sum*)))
+  (> *calculate-index* (calculate-max-index)))
 
-(defun calculate-check-result (c r)
-  (let ((expected (reduce '+ (nth 0 c))))
+(defun calculate-check-result (r)
+  (let ((expected (reduce '+ (nth 0 *calculate-sum*))))
     (= expected r)))
 
 (defun calculate ()
@@ -84,18 +84,19 @@
 
 (defun calculate-get-result ()
   (let* ((m (nth 1 *calculate-sum*))
-        (cols (matrix-cols m))
-        (rows (matrix-rows m))
-        (r 0))
+         (cols (matrix-cols m))
+         (rows (matrix-rows m))
+         (r 0))
     (dotimes (c cols)
       (setq r (+ (* r 10) (or (matrix-get m c (- rows 1))
                              0))))
     r))
 
 (defun calculate-init ()
+  (interactive)
   (setq *calculate-sum* (calculate-new-sum (list 1 23 456 6789)))
   (setq *calculate-index* 0)
-  (calculate-render *calculate-sum*))
+  (calculate-render))
 
 (define-derived-mode calculate-mode special-mode "calculate"
   (define-key calculate-mode-map (kbd "0") 'calculate-set-0)
@@ -108,13 +109,13 @@
   (define-key calculate-mode-map (kbd "7") 'calculate-set-7)
   (define-key calculate-mode-map (kbd "8") 'calculate-set-8)
   (define-key calculate-mode-map (kbd "9") 'calculate-set-9)
-  )
+  (define-key calculate-mode-map (kbd "r") 'calculate-init))
 
 (defun calculate-set-entered-value (v)
   (when (not (calculate-finishedp))
-    (calculate-set-value *calculate-sum* *calculate-index* v)
+    (calculate-set-value *calculate-index* v)
     (setq *calculate-index* (+ 1 *calculate-index*))
-    (calculate-render *calculate-sum*)))
+    (calculate-render)))
 
 (defun calculate-set-0 () (interactive) (calculate-set-entered-value 0))
 (defun calculate-set-1 () (interactive) (calculate-set-entered-value 1))
@@ -126,17 +127,5 @@
 (defun calculate-set-7 () (interactive) (calculate-set-entered-value 7))
 (defun calculate-set-8 () (interactive) (calculate-set-entered-value 8))
 (defun calculate-set-9 () (interactive) (calculate-set-entered-value 9))
-
-;; ;; testscript
-;; (setq abc (list 1 23 456 6789))
-;; (calculate-check-result (calculate-new-sum abc) 7260) ; nil
-;; (calculate-check-result (calculate-new-sum abc) 7269) ; 't
-;; (setq cal (calculate-new-sum abc))
-;; (calculate-render cal)
-
-;; (dotimes (i 9)
-;;   (calculate-set-value cal i i))
-
-;; (calculate-set-value cal 9 9)
 
 (provide 'calculate)
