a302c034bb8ecf814e6de8440eb384f0ae0a5e67
Peterpaul Klein Haneveld <peterpaulk@devpp>
2018-11-29 08:37:28 +0100

    Switch modeline to spaceline

--- lisp/customize-modeline.el
+++ lisp/customize-modeline.el
@@ -6,53 +6,43 @@
   ;;   (sml/setup)
   ;;   )
 
-  (use-package powerline
-    :config
-    (progn
-      (setq powerline-utf-8-separator-left        #xe0b0
-	    powerline-utf-8-separator-right       #xe0b2
-	    airline-utf-glyph-separator-left      #xe0b0
-	    airline-utf-glyph-separator-right     #xe0b2
-	    airline-utf-glyph-subseparator-left   #xe0b1
-	    airline-utf-glyph-subseparator-right  #xe0b3
-	    airline-utf-glyph-branch              #xe0a0
-	    airline-utf-glyph-readonly            #xe0a2
-	    airline-utf-glyph-linenumber          #xe0a1)
-      (powerline-default-theme)
-      ))
+  ;; (use-package powerline
+  ;;   :config
+  ;;   (progn
+  ;;     (setq powerline-utf-8-separator-left        #xe0b0
+  ;;           powerline-utf-8-separator-right       #xe0b2
+  ;;           airline-utf-glyph-separator-left      #xe0b0
+  ;;           airline-utf-glyph-separator-right     #xe0b2
+  ;;           airline-utf-glyph-subseparator-left   #xe0b1
+  ;;           airline-utf-glyph-subseparator-right  #xe0b3
+  ;;           airline-utf-glyph-branch              #xe0a0
+  ;;           airline-utf-glyph-readonly            #xe0a2
+  ;;           airline-utf-glyph-linenumber          #xe0a1)
+  ;;     (powerline-default-theme)
+  ;;     ))
 
-  (use-package airline-themes
-    :config
-    (load-theme 'airline-solarized-gui)
-    )
+  ;; (use-package airline-themes
+  ;;   :config
+  ;;   (load-theme 'airline-solarized-gui)
+  ;;   )
   
   ;; (defvar use-fancy-spaceline (y-or-n-p-with-timeout "Use fancy spaceline-all-the-icons?" 3 nil))
-  ;; (defvar use-fancy-spaceline nil)
+  (defvar use-fancy-spaceline 't)
 
-  ;; (use-package spaceline
-  ;;   :config
-  ;;   (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
+  (use-package spaceline
+    :config
+      (spaceline-emacs-theme))
 
-  ;; (use-package spaceline-config
-  ;;   :config
-  ;;   (unless use-fancy-spaceline
-  ;;     ;;(spaceline-spacemacs-theme)
-  ;;     (spaceline-emacs-theme)
-  ;;     )
-  ;;   )
-
-  ;; (when use-fancy-spaceline
-  ;;   (use-package spaceline-all-the-icons
-  ;;     :after spaceline
-  ;;     :config
-  ;;     (spaceline-all-the-icons-theme)
-  ;;     (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
-  ;;     (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
-  ;;     (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
-  ;;     (setq spaceline-all-the-icons-separator-type (quote wave))
-  ;;     )
-  ;;   )
-  )
+  (use-package spaceline-all-the-icons
+    :after spaceline
+    :config
+    (progn
+      (require 'package)
+      (spaceline-all-the-icons-theme)
+      (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
+      (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
+      (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
+  )))
 
 (provide 'customize-modeline)
 ;;; customize-modeline ends here
