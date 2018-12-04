42b6e41dfcada8bc48750a5d4a531a999cbb3480
Peterpaul Klein Haneveld <peterpaulk@devpp>
2018-12-04 11:55:14 +0100

    Allow opening multiple keystore files

--- lisp/keytool-mode.el
+++ lisp/keytool-mode.el
@@ -10,26 +10,31 @@
 (defun keytool-init (file)
   "Initialize a new keytool buffer with a file."
   (interactive "fKeystore File: ")
-  (setq *keystore-filename* file)
-  (setq *keystore-password* (read-passwd "Keystore Password: "))
   (message "Opening keystore: '%s'" file)
-  (switch-to-buffer "keytool")
+  (switch-to-buffer file)
   (keytool-mode)
+  (make-local-variable 'keystore-filename)
+  (setq keystore-filename file)
   (origami-mode)
-  (keytool-render)
-  (goto-char (point-min))
-  (origami-close-all-nodes (get-buffer "keytool"))
-  )
+  (keytool-render))
 
 (defun keytool-render ()
   "Render keytool"
   (interactive)
-  (let ((inhibit-read-only t))
-    (erase-buffer)
-    (insert (shell-command-to-string (format "keytool -list -keystore '%s' -storepass '%s' -rfc"
-                                             *keystore-filename*
-                                             *keystore-password*)))
-    ))
-
+  (when keystore-filename
+    (let ((inhibit-read-only t)
+          (keystore-password (read-passwd "Keystore Password: ")))
+      (erase-buffer)
+      (insert
+       (shell-command-to-string
+        (format "keytool -list -keystore '%s' -storepass '%s' -rfc"
+                keystore-filename
+                keystore-password)))
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward "\n\n+" nil t)
+          (replace-match "\n\n" nil nil)))
+      (goto-char (point-min))
+      (origami-close-all-nodes (get-buffer keystore-filename)))))
 
 (provide 'keytool-mode)
