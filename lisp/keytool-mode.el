defb94740ee7f0febe5271379121b2d0c2e11d60
Peterpaul Klein Haneveld <peterpaulk@devpp>
2018-12-06 21:10:04 +0100

    Easily switch between 3 representations, import certificate to keystore

--- lisp/keytool-mode.el
+++ lisp/keytool-mode.el
@@ -1,9 +1,88 @@
 ;;; -*- lexical-binding: t; -*-
 (require 'origami)
 
+(setq keytool-syntax-constants
+      '(
+        "Alias name"
+        "AuthorityInfoAccess"
+        "AuthorityKeyIdentifier"
+        "BasicConstraints"
+        "CRLDistributionPoints"
+        "Certificate chain length"
+        "Certificate fingerprint"
+        "Certificate fingerprints"
+        "CertificatePolicies"
+        "CertificatePolicyId"
+        "Certificate"
+        "Creation date"
+        "DistributionPoint"
+        "Entry type"
+        "ExtendedKeyUsages"
+        "Extensions"
+        "Issuer"
+        "KeyIdentifier"
+        "KeyUsage"
+        "Keystore provider"
+        "Keystore type"
+        "ObjectId"
+        "Owner"
+        "PolicyQualifierInfo"
+        "Serial number"
+        "Signature algorithm name"
+        "Subject Public Key Algorithm"
+        "SubjectAlternativeName"
+        "SubjectKeyIdentifier"
+        "URIName"
+        "Valid from"
+        "Version"
+        "accessLocation"
+        "accessMethod"
+        "qualifierID"
+        "qualifier"
+        "until"
+        ))
+
+(setq keytool-syntax-keywords
+      '(
+        "[0-9A-F][0-9A-F]\\(:[0-9A-F][0-9A-F]\\)+"
+        "[0-9]+\\([\\.][0-9]+\\)+"
+        "[0-9]+:\\( +[0-9A-F][0-9A-F]\\)+.*$"
+        "caIssuers"
+        "ocsp"
+        "serverAuth"
+        "clientAuth"
+        "DigitalSignature"
+        "Key_Encipherment"
+        "DNSName"
+        "trustedCertEntry"
+        "PrivateKeyEntry"
+        "MD5"
+        "SHA1withRSA"
+        "SHA1"
+        "SHA256withRSA"
+        "SHA256"
+        "SHA384withRSA"
+        "SHA384"
+        ))
+
+(defun keytool-reduce-list-of-strings-to-regex (list-of-strings)
+  (seq-reduce (lambda (x y) (format "%s\\|%s" x y))
+              (cdr list-of-strings)
+              (car list-of-strings)))
+
+(setq keytool-highlights
+      (list (cons (keytool-reduce-list-of-strings-to-regex keytool-syntax-constants)
+                  font-lock-constant-face)
+            (cons (keytool-reduce-list-of-strings-to-regex keytool-syntax-keywords)
+                  font-lock-keyword-face)))
+
 (define-derived-mode keytool-mode special-mode "keytool"
   (define-key keytool-mode-map (kbd "<tab>") 'origami-recursively-toggle-node)
-  )
+  (define-key keytool-mode-map (kbd "d") 'keytool-render)
+  (define-key keytool-mode-map (kbd "v") 'keytool-render-verbose)
+  (define-key keytool-mode-map (kbd "r") 'keytool-render-rfc)
+  (define-key keytool-mode-map (kbd "q") 'kill-this-buffer)
+  (setq font-lock-defaults '(keytool-highlights)))
 
 (map-put origami-parser-alist 'keytool-mode (origami-markers-parser "-----BEGIN CERTIFICATE-----" "-----END CERTIFICATE-----"))
 
@@ -14,27 +93,66 @@
   (switch-to-buffer file)
   (keytool-mode)
   (make-local-variable 'keystore-filename)
-  (setq keystore-filename file)
+  (setq keystore-filename file) 
+  (make-local-variable 'keystore-passphrase)
+  (setq keystore-passphrase nil)
   (origami-mode)
   (keytool-render))
 
-(defun keytool-render ()
-  "Render keytool"
-  (interactive)
+(defun keytool-get-passphrase-with-prompt (prompt)
+  (read-passwd (format "%s (%s): "
+                       prompt
+                       keystore-filename)))
+
+(defun keytool-get-passphrase-lazy ()
+  (when (not keystore-passphrase)
+    (setq keystore-passphrase
+          (keytool-get-passphrase-with-prompt "Keystore Passphrase")))
+  keystore-passphrase)
+
+(defun keytool-render-style (style)
   (when keystore-filename
     (let ((inhibit-read-only t)
-          (keystore-password (read-passwd "Keystore Password: ")))
+          (keystore-password (keytool-get-passphrase-lazy)))
       (erase-buffer)
       (insert
        (shell-command-to-string
-        (format "keytool -list -keystore '%s' -storepass '%s' -rfc"
+        (format "keytool -list -keystore '%s' -storepass '%s' %s"
                 keystore-filename
-                keystore-password)))
-      (save-excursion
-        (goto-char (point-min))
-        (while (re-search-forward "\n\n+" nil t)
-          (replace-match "\n\n" nil nil)))
+                keystore-password
+                style)))
+      (goto-char (point-min))
+      (while (re-search-forward "\n\n+" nil t)
+        (replace-match "\n\n" nil nil))
       (goto-char (point-min))
       (origami-close-all-nodes (get-buffer keystore-filename)))))
+
+(defun keytool-render ()
+  (interactive)
+  (keytool-render-style ""))
+
+(defun keytool-render-verbose ()
+  (interactive)
+  (keytool-render-style "-v"))
+
+(defun keytool-render-rfc ()
+  (interactive)
+  (keytool-render-style "-rfc"))
+
+(defun keytool-importcert (cert-buffer cert-alias)
+  "Import certificate from CERT-BUFFER with alias CERT-ALIAS."
+  (interactive "bBuffer with certificate to import: \nsSet alias for certificate: ")
+  (let ((keystore-file keystore-filename)
+        (keystore-pass (keytool-get-passphrase-with-prompt (format "Enter password to import certificate from '%s'"
+                                                                   cert-buffer))))
+    (save-excursion
+      (set-buffer cert-buffer)
+      (shell-command-on-region (point-min)
+                               (point-max)
+                               (format "keytool -importcert -keystore '%s' -storepass '%s' -alias '%s' -noprompt"
+                                       keystore-file
+                                       keystore-pass
+                                       cert-alias)))
+    (keytool-render)))
 
 (provide 'keytool-mode)
