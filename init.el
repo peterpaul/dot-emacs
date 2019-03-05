3aee2c5f0f2696d2f4249b7395439e3fb2c75a48
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2019-03-05 14:22:25 +0100

    Update xrandr config

--- init.el
+++ init.el
@@ -782,14 +782,14 @@ PARAMS progress report notification data."
 
 (defun peterpaulk/xrandr-desk ()
   (interactive)
-  (start-process-shell-command "xrandr" nil "--output eDP-1 --off --output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off")
-  (setq exwm-randr-workspace-monitor-plist '(1 "HDMI-1" 2 "HDMI-2"))
+  (start-process-shell-command "xrandr" nil "--output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off")
+  (setq exwm-randr-workspace-monitor-plist '(0 "eDP-1" 1 "HDMI-1" 2 "HDMI-2"))
   (exwm-randr-refresh))
 
 (defun peterpaulk/xrandr-laptop ()
   (interactive)
   (start-process-shell-command "xrandr" nil "--output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off")
-  (setq exwm-randr-workspace-monitor-plist '(1 "eDP-1"))
+  (setq exwm-randr-workspace-monitor-plist '(0 "eDP-1"))
   (exwm-randr-refresh))
 
 (use-package popwin
