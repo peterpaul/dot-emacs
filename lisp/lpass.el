5ff63fdd29ad53ebe953f684a3bd2a4601fd7a18
Peterpaul Taekele Klein Haneveld <peterpaul@zenbook.home>
2019-04-14 11:52:44 +0200

    [lpass] Add customization options

--- lisp/lpass.el
+++ lisp/lpass.el
@@ -1,3 +1,4 @@
+;;; lpass.el --- Lastpass mode
 ;;; -*- lexical-binding: t; -*-
 ;;
 ;; Problems with emacs-lastpass.el
@@ -5,20 +6,53 @@
 ;; - password is printed to message
 ;; - it is too easy to accidentally delete an account from the list-all view
 
-(defcustom lpass-list--format
-  (list '("ID" 20 nil)
-        '("Group" 32 t)
-        '("Username" 32 t)
-        '("Name" 64 t))
+(defconst lpass-identifiers (list '(:id . ("%ai" "account id"))
+                                  '(:name . ("%an" "account name"))
+                                  '(:name-including-path . ("%aN" "account name including path"))
+                                  '(:user . ("%au" "account user"))
+                                  '(:password . ("%ap" "account password"))
+                                  '(:modification-time . ("%am" "account modification time"))
+                                  '(:last-touch-time . ("%aU" "account last touch time"))
+                                  '(:share-name . ("%as" "account share name"))
+                                  '(:group-name . ("%ag" "account group name"))
+                                  '(:URL . ("%al" "account URL")))
+  "Column identifiers for tabulated-list.")
+
+(defconst lpass-identifiers-customize-choice (cons 'choice
+                                                   (mapcar (lambda (x) (list 'const x))
+                                                           (mapcar (lambda (x) (car x))
+                                                                   lpass-identifiers)))
+  "Choice of identifiers for customize.")
+
+(defvar-local lpass-current-group nil
+  "Current selected account group.
+When nil all groups are shown, when set only entries from the
+selected group are shown.")
+
+(defcustom lpass-list-format
+  (list '(:id 20 nil)
+        '(:group-name 32 t)
+        '(:user 32 t)
+        '(:name 64 t))
   "Format for tabulated-list."
   :group 'lpass
-  :type '(repeat (list (string :tag "Column name")
+  :type `(repeat (list ,lpass-identifiers-customize-choice
                        (integer :tag "Column width")
                        (choice (boolean :tag "Sort by value?")
                                (function :tag "Sorting predicate")))))
 
-(defvar-local lpass-current-group nil
-  "Current selected group.")
+(defcustom lpass-list-default-sort-column
+  :group-name
+  "Default sort column of tabulated-list."
+  :group 'lpass
+  :type lpass-identifiers-customize-choice)
+
+(defun lpass-mode-format-string ()
+  "Generate the format string for tabulated list from `lpass-list-format'."
+  (vconcat (mapcar (lambda (x) (cons
+                                (nth 1 (alist-get (car x) lpass-identifiers))
+                                (cdr x)))
+                   lpass-list-format)))
 
 (defun lpass-status ()
   "Checks whether a user is logged in.
@@ -47,13 +81,14 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
   (let (entries)
     (with-current-buffer buffer
       (goto-char (point-max))
-      (while (re-search-backward "^\\([^~]*\\)~\\([^~]*\\)~\\([^~]*\\)~\\([^~]*\\)$" nil t)
+      (while (re-search-backward (format "^\\([^~]*\\)~%s$"
+                                         (s-join "~"
+                                                 (mapcar (lambda (x) "\\([^~]*\\)")
+                                                         lpass-list-format)))
+                                 nil t)
         (setq entries (cons (list (match-string 1)
-                                  (vector (match-string 1)
-                                          (match-string 2)
-                                          (match-string 3)
-                                          (match-string 4)
-                                          ))
+                                  (vconcat (mapcar (lambda (x) (match-string x))
+                                                   (number-sequence 2 (+ -1 2 (seq-length lpass-list-format))))))
                             entries))))
     entries))
 
@@ -62,8 +97,13 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
   (interactive)
   (unless (lpass-status)
     (error "Not logged in"))
-  (let ((cmd (format "lpass ls --format=\"%s\" %s"
-                     "%ai~%ag~%an~%au"
+  (let ((cmd (format "lpass ls --format=\"%s~%s\" %s"
+                     "%ai"
+                     (s-join "~"
+                             (mapcar (lambda (x) (nth 0
+                                                      (alist-get (car x)
+                                                                 lpass-identifiers)))
+                                     lpass-list-format))
                      (shell-quote-argument (or lpass-current-group
                                                "")))))
     (with-temp-buffer
@@ -75,7 +115,7 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
 (defvar lpass-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map tabulated-list-mode-map)
-    (define-key map "w" #'lpass-copy-password)
+    (define-key map "w" #'lpass-list-copy-password)
     map)
   "Local keymap for `lpass-mode' buffers.")
 
@@ -83,9 +123,9 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
   "\\<keystore-mode-map>
 \\{keystore-mode-map}"
   ;; TODO before refresh of contents, reset tabulated-list-format
-  (setq-local tabulated-list-format (vconcat lpass-list--format))
+  (setq-local tabulated-list-format (lpass-mode-format-string))
   (setq-local tabulated-list-padding 2)
-  (setq-local tabulated-list-sort-key (cons "Group" nil))
+  (setq-local tabulated-list-sort-key (cdr (alist-get lpass-list-default-sort-column lpass-identifiers)))
   (setq-local tabulated-list-entries #'lpass-list-entries)
   (tabulated-list-init-header))
 
@@ -94,27 +134,27 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
   (elt (tabulated-list-get-entry (point)) column-index))
 
 (defun lpass-list--limit-group (pos)
-  ""
+  "Set `lpass-current-group' to the group for the account at POS."
   (interactive "d")
   (setq-local lpass-current-group (lpass-list--column-value 1))
   (tabulated-list-revert))
 
 (defun lpass-list--reset-group ()
-  ""
+  "Clear `lpass-current-group'."
   (interactive)
   (setq-local lpass-current-group nil)
   (tabulated-list-revert))
 
 (defun lpass-list ()
-  ""
+  "Open buffer with lastpass entries."
   (interactive)
   (with-current-buffer (get-buffer-create "*lpass list*")
     (lpass-mode)
     (tabulated-list-print t)
     (switch-to-buffer (current-buffer))))
 
-(defun lpass-show (pos)
-  ""
+(defun lpass-list-show (pos)
+  "Show details of lastpass account at POS."
   (interactive "d")
   (let ((id (tabulated-list-get-id pos)))
     (with-current-buffer (get-buffer-create "*lpass show*")
@@ -123,8 +163,8 @@ BUFFER can be a buffer or a buffer name, and should contain the output of 'lpass
                      "*lpass errors*")
       (switch-to-buffer (current-buffer)))))
 
-(defun lpass-copy-password (pos)
-  ""
+(defun lpass-list-copy-password (pos)
+  "Add the password of the account at POS to the kill-ring."
   (interactive "d")
   (let ((id (tabulated-list-get-id pos)))
     (with-temp-buffer
