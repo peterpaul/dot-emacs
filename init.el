6ff73dd2251e55bd28dd185119b98c6a3b55cf63
Peterpaul Taekele Klein Haneveld <peterpaul.klein.haneveld@connectis.com>
2019-09-16 11:27:03 +0200

    Install org-brain

--- init.el
+++ init.el
@@ -341,6 +341,21 @@ will only work on systems where the command =which= exists."
 
 (use-package org-preview-html)
 
+(use-package org-brain
+  :init
+  (setq org-brain-path "~/Documents/brain")
+  ;; For Evil users
+  ;; (with-eval-after-load 'evil
+  ;;   (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  ;; (push '("b" "Brain" plain (function org-brain-goto-end)
+  ;;         "* %i%?" :empty-lines 1)
+  ;;       org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12))
+
 (use-package htmlize)
 
 (use-package epresent
