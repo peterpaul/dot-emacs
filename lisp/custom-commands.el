b1b6eeddc35e91b5c94068f75e0e86f1dfd8f32e
Peterpaul Taekele Klein Haneveld <peterpaul.klein.haneveld@connectis.nl>
2020-02-03 08:31:24 +0100

    Add unfill commands

--- lisp/custom-commands.el
+++ lisp/custom-commands.el
@@ -4,5 +4,28 @@
   (interactive)
   (async-shell-command "nmcli con up id dc02"))
 
+    ;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
+;; Handy key definition
+(define-key global-map "\C-\M-Q" 'unfill-region)
+
 (provide 'custom-commands)
 ;;; custom-commands ends here
